17dc35e80a3f94a6978fc8116428e5c59dc051d8
徐弋洋 <1315338860@qq.com>
2026-03-05 03:19:33 +0800

    删除 Huazhi0301

--- Huazhi0301/src/types/index.ts
+++
@@ -1,4 +0,0 @@
-export type { Skill, SubSkill, SkillCategory } from './skill'
-export type { Company, CompanyDetail, CompanyStatus, ScenarioStep } from './company'
-export type { KpiIndicator } from './kpi'
-export type { DashboardConfig } from './config'
